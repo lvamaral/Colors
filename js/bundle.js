23aaf5b446dacd612122218b08898a24c0c46241
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-07-14 16:48:46 -0700

    Optimized for Mobile

--- js/bundle.js
+++ js/bundle.js
@@ -488,6 +488,13 @@ $( () => {
     new __WEBPACK_IMPORTED_MODULE_3__level__["a" /* default */](1)
 });
 
+$('body').bind('touchend', function(e) {
+  e.preventDefault();
+  // Add your code here.
+  $(this).click();
+  // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
+})
+
 
 /***/ }),
 /* 5 */
